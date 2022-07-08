b9dbdc812bab82bc765941e09fac47585b2c8468
YevhenKap <kapelianovych.y.v@gmail.com>
2022-07-08 14:07:13 +0300

    Remove the datalist element from the Home page.

--- src/pages/Home/components/FirstView/index.jsx
+++ src/pages/Home/components/FirstView/index.jsx
@@ -1,7 +1,4 @@
-import { useId } from "react";
-
 import Icon from "../../../../components/icons/index.jsx";
-import { useBooks } from "../../../../cache/books.jsx";
 import { useSearch } from "../../../../cache/search.jsx";
 
 import {
@@ -15,8 +12,6 @@ import {
 } from "./index.module.css";
 
 export default () => {
-  const searchIdList = useId();
-  const [books] = useBooks();
   const [search, setSearch] = useSearch();
 
   return (
@@ -37,7 +32,6 @@ export default () => {
         }}
       >
         <input
-          list={searchIdList}
           type="search"
           autoFocus
           name="search"
@@ -49,11 +43,6 @@ export default () => {
             currentTarget.value.length === 0 && setSearch("")
           }
         />
-        <datalist id={searchIdList}>
-          {books.map(({ id, title }) => (
-            <option key={id} value={title} />
-          ))}
-        </datalist>
         <button className={cSearchButton} type="submit">
           <Icon name="search" />
         </button>
