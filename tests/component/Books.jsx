9cbbee3901c054e7ecf45174ca77962ed6a0ad91
YevhenKap <kapelianovych.y.v@gmail.com>
2022-07-11 16:21:18 +0300

    Remove UserProvider from test component.

--- tests/component/Books.jsx
+++ tests/component/Books.jsx
@@ -1,12 +1,11 @@
 import BooksProvider from "/src/cache/books.jsx";
 import CartProvider from "/src/cache/cart.jsx";
-import UserProvider from "/src/cache/user.jsx";
 import Arrivals from "/src/pages/Home/components/Arrivals/index.jsx";
 
-export default () =>
-  <UserProvider>
-    <CartProvider>
-      <BooksProvider><Arrivals /></BooksProvider>
-    </CartProvider>
-  </UserProvider>
-
+export default () => (
+  <CartProvider>
+    <BooksProvider>
+      <Arrivals />
+    </BooksProvider>
+  </CartProvider>
+);
